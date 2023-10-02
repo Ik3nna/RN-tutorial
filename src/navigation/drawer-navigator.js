f1d79c25d9e4c587a057e334304d2f47275eb081
ik3nna <iamiykendukwu@gmail.com>
2023-10-02 19:36:54 +0100

    feat: Drawer component

--- src/navigation/drawer-navigator.js
+++ src/navigation/drawer-navigator.js
@@ -1,17 +1,73 @@
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import HomeNavigator from './home-navigator';
-import { HOME } from '../constants/routeName';
+import { HOME, LOGIN, SETTINGS } from '../constants/routeName';
+import { View, Text, Image, SafeAreaView, StyleSheet, TouchableOpacity } from 'react-native';
+import Container from '../components/common/container';
+
+// images
+import logo from "../assets/images/logo.png"
 
 const Drawer = createDrawerNavigator();
+
+const NavItems = ({ navigation }) => {
+  const menuItems = [
+    {icon:<Text>T</Text>, name:"Settings", onPress:()=>{navigation.navigate(SETTINGS)}},
+    {icon:<Text>T</Text>, name:"Logout", onPress:()=>{}}
+  ]
+
+  return (
+    <SafeAreaView>
+      <Container>
+        <Image style={styles.img} source={logo} alt="logo" />
+
+        <View style={{ paddingHorizontal: 70 }}>
+          {menuItems.map((item, index)=>
+            <TouchableOpacity key={index} style={styles.menuItems} onPress={item.onPress}>
+              {item.icon}
+
+              <Text style={styles.text}>{item.name}</Text>
+            </TouchableOpacity>
+          )}
+        </View>
+      </Container>
+    </SafeAreaView>
+  )
+};
+
+const getDrawerContent = (navigation) => {
+  return <NavItems navigation={navigation} />
+}
 
 export default function DrawerNavigator() {
   return (
     <Drawer.Navigator 
+      drawerContent={({ navigation })=>getDrawerContent(navigation)}
       screenOptions={{
-        headerShown: false
+        headerShown: false,
+        drawerType: "slide",
       }}
     >
       <Drawer.Screen name={HOME} component={HomeNavigator} />
     </Drawer.Navigator>
   );
 }
+
+
+const styles = StyleSheet.create({
+  img: {
+    width: 150,
+    height: 150,
+    alignSelf: "center",
+    marginTop: 50,
+  },
+  menuItems: {
+    flexDirection: "row",
+    // justifyContent: "center",
+    alignItems: "center"
+  },
+  text: {
+    fontSize: 17,
+    paddingVertical: 7,
+    paddingLeft: 20
+  }
+})
--- src/screens/contacts/index.js
+++ src/screens/contacts/index.js
@@ -1,8 +1,20 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import React, { useEffect } from 'react'
 import Container from '../../components/common/container'
+import { useNavigation } from '@react-navigation/native'
 
 export default function Contacts () {
+  const { setOptions, toggleDrawer } = useNavigation();
+
+  useEffect(()=>{
+    setOptions({
+      headerLeft: ()=> 
+      <TouchableOpacity onPress={()=>toggleDrawer()}>
+        <Text style={{padding:10}}>Nav</Text>
+      </TouchableOpacity>
+    })
+  },[]);
+
   return (
     <Container>
       <Text>Contacts</Text>
